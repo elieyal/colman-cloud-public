db7975ef7991a6d7534248298fc3fbe84592f6e6
eli <eli@eli.com>
2024-05-01 12:11:01 +0300

    class 9

---
+++ class 9 - Terraform 2/terraform-demo2/init.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "<html><H1>this is my website!!. this server ip is `curl ident.me --silent`</h1></html>" > /usr/share/nginx/html/index.html
---
+++ class 9 - Terraform 2/terraform-demo2/variables-locals.tf
@@ -0,0 +1,10 @@
+locals {
+  common_tags = {
+      owner = "Eli"
+      usage = "demo2"
+  }
+  vpc_name       = "colman-cloud-demo2"
+  vpc_cidr_block = "10.10.0.0/16"
+  public_subnets = ["10.10.0.0/20", "10.10.16.0/20", "10.10.32.0/20"]
+  ami = "ami-0004c36d1930a15e6"
+}
